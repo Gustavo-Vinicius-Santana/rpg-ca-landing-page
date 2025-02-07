bcfb887d8418e9f3c2b7a4624713cb4ef3f0d4a6
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-02-07 00:29:40 -0300

    [feature - ui/logic - complet] (componentizando header) Componentizando header e adicionando animações ao passar o mouse em cima das opções de navegação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Image from 'next/image';
-import Link from 'next/link'
+import Header from "@/ui/templates/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,39 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="flex items-center justify-between p-4">
-          <div className="flex items-center gap-12">
-            <div className="flex items-center gap-2">
-              <Image src="/logo.png" alt="logo" width={50} height={50} />
-              <h1 className="font-bold text-3xl">RPG CA</h1>
-            </div>
-
-            <div className="flex items-center">
-              <ul className="flex gap-8">
-                <li className=" text-2xl"><Link href="/">Home</Link></li>
-                <li className="text-2xl"><Link href="/pages/versions">Versões</Link></li>
-                <li className="text-2xl"><Link href="/pages/devlog">Devlog</Link></li>
-                <li className="text-2xl"><Link href="/pages/contact">Contato</Link></li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4">
-
-            <Link href="/pages/download">
-              <button className="bg-white text-black px-4 py-2 rounded-md shadow-sm hover:bg-gray-100 hover:scale-105 transition duration-200">
-                <p className="text-sm font-semibold">Download</p>
-              </button>
-
-            </Link>
-
-            <Link href="/pages/collaborate">
-              <button  className="bg-white text-black px-4 py-2 rounded-md shadow-sm hover:bg-gray-100 hover:scale-105 transition duration-200">
-                <p className="text-sm font-semibold">Colabore</p>
-              </button>
-            </Link>
-          </div>
-        </header>
+      <Header 
+        home="/" 
+        versions="/pages/versions" 
+        devlog="/pages/devlog" 
+        contact="/pages/contact" 
+        download="/pages/download" 
+        collaborate="/pages/collaborate" 
+      />
 
         <main>
           {children}
